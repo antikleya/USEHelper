dd94017a6e510859baf1fb1ec078d0498facce28
Гусева Анастасия ИУ6-34Б <nesty.8822@ya.ru>
2022-12-03 15:17:29 +0300

    add frontend

---
+++ frontend/my-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+export function App() {
+  return (
+    <div className="App">
+      <header>
+      <nav class="navbar navbar-light bg-light">
+        <form class="form-inline">
+           <button class="btn btn-outline-success" type="button">Main button</button>
+           <button class="btn btn-sm btn-outline-secondary" type="button">Smaller button</button>
+        </form>
+</nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
