4ed3d6cf5ff6dc2d6b6fa547da47d4b874b4534d
Гусева Анастасия ИУ6-34Б <nesty.8822@ya.ru>
2022-12-04 14:03:31 +0300

    add login form

--- frontend/my-app/src/components/Login.js
+++ frontend/my-app/src/components/Login.js
@@ -1,5 +1,32 @@
+import "./Login.css";
+
 export const Login = () => {
-    return(
-        <p>Hello in Login!</p>
-    );
-}
+    return (
+    <h1>
+      <form className="loginForm">
+        <h2>Авторизация</h2>
+        <div>
+          <input
+            className="loginFormInput"
+            type="text"
+            placeholder="Логин"
+            required
+          />
+        </div>
+        <div>
+          <input
+            className="loginFormInput"
+            type="password"
+            placeholder="Пароль"
+            required
+          />
+        </div>
+        <div>
+          <button className="blackBtn" type="submit">
+            Войти
+          </button>
+        </div>
+      </form>
+    </h1>
+  );
+};
