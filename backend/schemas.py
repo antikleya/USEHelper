b848d572afdc468c5bdc5ada45b048ddefc9a3bd
antikleya <anitkleya@yandex.ru>
2022-12-07 15:35:36 +0300

    added Thema and Teacher schemas. Refactored schemas

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,8 +1,10 @@
 import datetime as _dt
+from typing import List
 
 import pydantic as _pydantic
 
 
+# ----------------BASE-MODELS------------------
 class _UserBase(_pydantic.BaseModel):
     email: str
     name: str
@@ -11,27 +13,47 @@ class _UserBase(_pydantic.BaseModel):
         orm_mode = True
 
 
-class UserCreate(_UserBase):
-    hashed_password: str
+class _TeacherBase(_pydantic.BaseModel):
+    name: str
+    phone_number: str
 
     class Config:
         orm_mode = True
+
+
+class _ThemeBase(_pydantic.BaseModel):
+    id: int
+    name: str
+    description: str
+
+    class Config:
+        orm_mode = True
+
+
+# -----------------------USER-MODELS-----------------------------------
+class UserCreate(_UserBase):
+    hashed_password: str
 
 
 class User(_UserBase):
     id: int
 
-    class Config:
-        orm_mode = True
 
-
-class _TeacherBase(_pydantic.BaseModel):
-    pass
+# ------------------------TEACHER-MODELS------------------------------
+class Teacher(_TeacherBase):
+    id: int
+    themes: List[_ThemeBase]
 
 
 class TeacherCreate(_TeacherBase):
+    themes: List[_ThemeBase]
+
+
+# ----------------------------THEME-MODELS-----------------------------
+class ThemeCreate(_ThemeBase):
     pass
 
 
-class TeacherUpdate(_TeacherBase):
-    pass
+class Theme(_ThemeBase):
+    id: int
+    teachers: List[_TeacherBase]
